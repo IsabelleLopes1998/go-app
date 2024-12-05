1e408bb18339d097abdcb139ed5074b418a4f46b
Isabelle Lopes <isabelle.biomed@gmail.com>
2024-12-05 16:41:25 -0300

    teste

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 )
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, World!!")
 }
 func main() {
 	http.HandleFunc("/", handler)
